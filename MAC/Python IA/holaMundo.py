35164007212ec03ac8ad10bc7e5c35db0356ab28
Rebellion12 <67783461+Rebellion12@users.noreply.github.com>
2024-11-25 17:56:14 +0100

    Add files via upload

---
+++ MAC/Python IA/holaMundo.py
@@ -0,0 +1,15 @@
+entero1 = 5
+entero2 = 4
+sumaEntero = entero1 + entero2
+restaEntero = entero1 - entero2
+factorEntero = entero1 * entero2
+divisionEntero = entero1 / entero2
+
+cad = "suma: " + str(sumaEntero)
+
+
+cadenaEnteros = "La suma de enteros es = " + str(sumaEntero)
+                 + "La resta de enteros es = " + restaEntero
+                 + "La multiplicación de enteros es = " + factorEntero
+                 + "La división de enteros es = " + divisionEntero
+print (cad)
